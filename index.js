8c70cf9c5a4358c3e4e080806728d94fd61181d3
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-01-14 21:34:28 -0500

    added userschema

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 
+dotenv.config()
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
+  console.log('connected to db!')
+)
 // Import routes
 
 const authRoute = require('./routes/auth')
 
-//Route Middlewares
+// Route Middlewares
 
 app.use('/api/user', authRoute)
 
