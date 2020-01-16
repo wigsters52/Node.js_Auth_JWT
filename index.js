c60bf31531619ecc4cbd268a4eb457cbec14c999
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-01-15 19:10:28 -0500

    Having trouble getting this to register to db, no errors are coming up, Postman is just hanging

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-
-dotenv.config()
-
-// Connect to DB
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
-  console.log('connected to db!')
-)
 // Import routes
 
 const authRoute = require('./routes/auth')
 
+dotenv.config()
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log('connected to db!')
+)
+
+// Middleware
+app.use(express.json())
+
 // Route Middlewares
 
-app.use('/api/user', authRoute)
+app.use('/api/user/', authRoute)
 
-app.listen(3000, () => console.log('Server up and running'))
+app.listen(5000, () => console.log('Server up and running'))
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,37 @@
 const router = require('express').Router()
+const User = require('../model/User')
 
-router.post('/register', (req, res) => {
-  res.send('Register')
+// Validation
+const Joi = require('@hapi/joi')
+
+const schema = Joi.object({
+  name: Joi.string()
+    .min(6)
+    .required(),
+  email: Joi.string()
+    .min(6)
+    .required()
+    .email(),
+  password: Joi.string()
+    .min(6)
+    .required()
+})
+
+router.post('/register', async (req, res) => {
+  // valiudate the data before making an user
+  const { error } = schema.validate(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  })
+  try {
+    const savedUser = await user.save()
+    res.send(savedUser)
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 router.post('/login')
