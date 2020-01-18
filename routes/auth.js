efcd0b4f4b001bf99c595a978ea6b914f36149f4
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-01-18 17:26:33 -0500

    seperated validation from auth file. added login validation for future use

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,10 @@
 const router = require('express').Router()
 const User = require('../model/User')
-
-// Validation
-const Joi = require('@hapi/joi')
-
-const schema = Joi.object({
-  name: Joi.string()
-    .min(6)
-    .required(),
-  email: Joi.string()
-    .min(6)
-    .required()
-    .email(),
-  password: Joi.string()
-    .min(6)
-    .required()
-})
+const { registerValidation } = require('../validation')
 
 router.post('/register', async (req, res) => {
   // valiudate the data before making an user
-  const { error } = schema.validate(req.body)
+  const { error } = registerValidation(req.body)
   if (error) return res.status(400).send(error.details[0].message)
   const user = new User({
     name: req.body.name,
