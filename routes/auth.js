edc84537668ba2ae980d8c12a025d40db8bc2e1d
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-01-19 19:16:00 -0500

    Added checks for email existing

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,11 @@ router.post('/register', async (req, res) => {
   // valiudate the data before making an user
   const { error } = registerValidation(req.body)
   if (error) return res.status(400).send(error.details[0].message)
+
+  // Check if the user is already in the database
+  const emailExists = await User.findOne({ email: req.body.email })
+  if (emailExists) return res.status(400).send('Email already exists')
+  // create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
